5c804455c7deb837db64daac37bdfac02b1344b2
IraHrechyn <hrechyniryna@gmail.com>
2024-12-11 17:10:23 +0200

    feat: create main project structure

---
+++ src/app/page/components/weather-cards/weather-cards.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-weather-cards',
+  standalone: true,
+  imports: [],
+  templateUrl: './weather-cards.component.html',
+  styleUrl: './weather-cards.component.css'
+})
+export class WeatherCardsComponent {
+
+}
---
+++ src/app/page/components/weather-sidebar/weather-sidebar.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-weather-sidebar',
+  standalone: true,
+  imports: [],
+  templateUrl: './weather-sidebar.component.html',
+  styleUrl: './weather-sidebar.component.css'
+})
+export class WeatherSidebarComponent {
+
+}
