cbf1edfcbd57ca9b4caf6da6dc8407bbcc222645
IraHrechyn <hrechyniryna@gmail.com>
2024-12-11 17:17:01 +0200

    feat: add to assets font and weather icons; add responsive design for WeatherSidebarComponent

--- src/app/page/components/weather-sidebar/weather-sidebar.component.ts
+++ src/app/page/components/weather-sidebar/weather-sidebar.component.ts
@@ -1,12 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild} from '@angular/core';
+import {CommonModule} from "@angular/common";
 
 @Component({
   selector: 'app-weather-sidebar',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule
+  ],
   templateUrl: './weather-sidebar.component.html',
-  styleUrl: './weather-sidebar.component.css'
+  styleUrls: ['./weather-sidebar.component.css', './../../styles/input-and-button.css']
 })
 export class WeatherSidebarComponent {
+  @ViewChild('cityContainer') cityContainer: ElementRef | undefined;
 
+  visibility: boolean = false;
+
+  visibilityCityContainer() {
+    this.visibility = !this.visibility;
+
+    const cityContainerElement = this.cityContainer?.nativeElement;
+
+    if (this.visibility) {
+      cityContainerElement.classList.remove('hidden');
+      cityContainerElement.classList.add('visible');
+    } else {
+      cityContainerElement.classList.add('hidden');
+      cityContainerElement.classList.remove('visible');
+    }
+
+    const button = document.querySelector('.toggle-button');
+    button?.classList.toggle('active');
+  }
 }
--- src/app/page/page.component.ts
+++ src/app/page/page.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import {WeatherSidebarComponent} from "./components/weather-sidebar/weather-sidebar.component";
+import {WeatherCardsComponent} from "./components/weather-cards/weather-cards.component";
 
 @Component({
   selector: 'app-page',
   standalone: true,
-  imports: [],
+  imports: [
+    WeatherSidebarComponent,
+    WeatherCardsComponent
+  ],
   templateUrl: './page.component.html',
   styleUrl: './page.component.css'
 })
